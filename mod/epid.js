25dfd18c490d24f32e384212695fa13cd7eee690
Luca Mari <lmari@liuc.it>
2020-03-31 22:41:01 +0200

    Model updated

--- mod/epid.js
+++ mod/epid.js
@@ -2,17 +2,18 @@ _model_data = {
   time0: 0, time1: 300, timeD: 1,
   parameters: [
     { id: "popolazione", val: 1 },
-
+    { id: "infettiIniz", val: 0.01 },
   ],
   variables: [
-    { id: "tassoInfez", eta: () => getFromWidget('tassoInfez') },
-    { id: "tassoGuarig", eta: () => getFromWidget('tassoGuarig') },
-    { id: "tassoMortal", eta: () => getFromWidget('tassoMortal') },
-    { id: "infettiAtt", phi: (nuoviInfetti,nuoviGuariti,nuoviMorti) => _this+nuoviInfetti-nuoviGuariti-nuoviMorti, init: 0.01 },
+    { id: "tassoInfez", eta: () => getFromWidget('Tasso di infezione') },
+    { id: "tassoGuarig", eta: () => getFromWidget('Tasso di guarigione') },
+    { id: "tassoMortal", eta: () => getFromWidget('Tasso di mortalità') },
+    { id: "infettiAtt", phi: (nuoviInfetti,nuoviGuariti,nuoviMorti) => min(1,_this+nuoviInfetti-nuoviGuariti-nuoviMorti), init: "infettiIniz" },
     { id: "nuoviGuariti", eta: (infettiAtt,tassoGuarig) => infettiAtt*tassoGuarig },
     { id: "nuoviMorti", eta: (infettiAtt,tassoMortal) => infettiAtt*tassoMortal },
-    { id: "nuoviInfetti", eta: (infettiAtt,tassoInfez,infettiTot,popolazione) => infettiAtt*tassoInfez*(1-infettiTot/popolazione) },
-    { id: "infettiTot", phi: (nuoviInfetti) => _this+nuoviInfetti, init: 0 },
+    { id: "nuoviInfetti", eta: (infettiAtt,tassoInfez,suscettibili,popolazione) => infettiAtt*tassoInfez*suscettibili/popolazione },
+    { id: "infettiTot", phi: (nuoviInfetti) => _this+nuoviInfetti, init: "infettiIniz" },
+    { id: "suscettibili", eta: (infettiTot, popolazione) => popolazione-infettiTot },
     { id: "mortiTot", phi: (nuoviMorti) => _this+nuoviMorti, init: 0 },
   ]
 };
@@ -20,9 +21,9 @@ _model_data = {
 _env_data = {
   simDelay: 10,
   trace: 0,
-  title: "Epidemia...",
+  title: 'Epidemia...',
   charts: [
-    { title: "Infetti totali", top: 70, left: 20, width: 500,
+    { title: 'Infetti totali', top: 70, left: 20, width: 500,
       xaxis: { min: 0, max: 300, step: 30 },
       yaxis: { min: 0 },
       series: "[{ x: Time, y: infettiTot }]",
@@ -34,19 +35,19 @@ _env_data = {
       lines: [{ show: true, color: 'red', width: 1 }, { show: true, color: 'blue', width: 1 }], },
   ],
   tables: [
-    { title: "mortiTot", top: 550, left: 620,
+    { title: 'mortiTot', top: 550, left: 620,
       series: "[mortiTot]",
       decimals: [3], alignments: ['center'], lastonly: [true] },
   ],
   sliders: [
-    { title: "tassoInfez", top: 550, left: 10, height: 100,
+    { title: 'Tasso di infezione', top: 550, left: 10, height: 100,
       min: 0, max: 1, step: 0.01,
-      tooltip: "Tasso di infezione" },
-    { title: "tassoGuarig", top: 550, left: 210, height: 100,
+      tooltip: 'Tasso di infezione' },
+    { title: 'Tasso di guarigione', top: 550, left: 210, height: 100,
       min: 0, max: 1, step: 0.01,
-      tooltip: "Tasso di guarigione" },
-    { title: "tassoMortal", top: 550, left: 410, height: 100,
+      tooltip: 'Tasso di guarigione' },
+    { title: 'Tasso di mortalità', top: 550, left: 410, height: 100,
       min: 0, max: 1, step: 0.01,
-      tooltip: "Tasso di mortalità" },
+      tooltip: 'Tasso di mortalità' },
   ],
 };
